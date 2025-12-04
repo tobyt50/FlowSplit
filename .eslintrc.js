f97484564ff093e14bfc9107e39f062db68c3794
Matthew Tedunjaiye <tedunjaiyem@gmail.com>
2025-12-04 16:10:58 +0100

    fix: relax eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
     root: true,
     extends: ['eslint:recommended', 'prettier'],
-    ignorePatterns: ['node_modules', 'dist'],
+    ignorePatterns: ['node_modules', 'dist', '.next', 'out'],
     rules: {
-        'no-console': 'warn',
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
 };
