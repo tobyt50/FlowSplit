7cb1182312bab3c8a953f2ae084b7ec603db89de
Matthew Tedunjaiye <tedunjaiyem@gmail.com>
2025-12-04 21:16:44 +0100

    Fixed web build errors

--- packages/apps/web/app/dashboard/overview/_components/ActiveRules.tsx
+++ packages/apps/web/app/dashboard/overview/_components/ActiveRules.tsx
@@ -2,7 +2,7 @@
 
 import React, { useMemo } from 'react';
 import { useRouter } from 'next/navigation';
-import { SplitRule, Wallet } from '@flowsplit/prisma';
+import type { SplitRule, Wallet } from '@flowsplit/prisma';
 import { SplitTypes } from '@/packages/apps/web/lib/enums';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '../../../../components/ui/Card';
 import { Switch } from '../../../../components/ui/Switch';
